aa29029a87ed025888300f9bbc07692e51a5fdb0
nguyendai-reddevil <dainguyenred2evil@gmail.com>
2021-03-31 09:14:15 +0700

    fix bug design

--- app/main/components/ProfileScreen/components/SelectHeightOrWeight/styles/index.css.js
+++ app/main/components/ProfileScreen/components/SelectHeightOrWeight/styles/index.css.js
@@ -28,7 +28,7 @@ const styles = StyleSheet.create({
   },
   textGender: {
     fontSize: fontSize.normal,
-    fontFamily: 'OpenSans-Regular'
+    fontFamily: 'OpenSans-Bold'
   },
   textLabel: {
     fontSize: fontSize.normal,
