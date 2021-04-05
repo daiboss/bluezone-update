eda7bdcf1a93c0e407be64566a2f0e9d29743d47
nguyendai-reddevil <dainguyenred2evil@gmail.com>
2021-04-05 17:24:07 +0700

    fix

--- app/main/components/StepHistoryScreen/index.ios.js
+++ app/main/components/StepHistoryScreen/index.ios.js
@@ -542,6 +542,64 @@ const StepCount = ({ props, intl, navigation }) => {
   }
 
 
+
+
+  const onGetStepLine = async () => {
+    let start = new Date();
+    let end = new Date();
+
+    start.setDate(start.getDate() - 7);
+    Fitness.getSteps({ startDate: start, endDate: end })
+      .then(res => {
+        // if (res.length) {
+        //   res.pop()
+        //   let data = res.map((obj, index) => ({
+        //     x: obj.quantity,
+        //     y: obj.quantity,
+        //   }));
+        //   let timeLine = res.map(obj => {
+        //     return new Date(obj.startDate).format('dd/MM')
+        //   })
+        //   // data.length !== 0 && data.pop()
+        //   setDataChart(data);
+
+        //   alert7dayLessThan1000(data)
+
+        //   setTime(timeLine)
+        // }
+          let startTimeStamp = moment(start).startOf('days').unix()
+          let endTimeStamp = moment(end).startOf('days').unix()
+
+          let p = parseInt((endTimeStamp - startTimeStamp) / 86400)
+
+          res.pop()
+          let data = res.map((obj, index) => ({
+            x: moment(obj.startDate).unix(),
+            y: obj.quantity,
+          }));
+          let listEmpty = []
+          for (let i = 0; i <= p; i++) {
+            listEmpty.push({
+              x: startTimeStamp + 86400 * i,
+              y: 0
+            })
+          }
+          let dataValue = [...listEmpty, ...data]
+          dataValue.length > 0 && dataValue.pop()
+
+          let timeLine = dataValue.map(obj => {
+            return moment.unix(obj.x).toDate().format('dd/MM')
+          })
+
+          setDataChart(dataValue);
+
+          alert7dayLessThan1000(data)
+
+          setTime(timeLine)
+      })
+      .catch(err => { });
+  };
+
   const onGetSteps = (start, end, type) => {
     try {
       Fitness.getSteps({ startDate: start, endDate: end })
