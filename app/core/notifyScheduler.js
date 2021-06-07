6381235601356598af11ba87012d599eae014e1e
nguyendai-reddevil <dainguyenred2evil@gmail.com>
2021-06-07 17:03:16 +0700

    fix bug

--- app/core/notifyScheduler.js
+++ app/core/notifyScheduler.js
@@ -615,7 +615,7 @@ export const createScheduleWarnningWeightNotification = async (time) => {
       playSound: false, // (optional) default: true
       soundName: 'default', // (optional) Sound to play when the notification is shown. Value of 'default' plays the default sound. It can be set to a custom sound such as 'android.resource://com.xyz/raw/my_sound'. It will look for the 'my_sound' audio file in 'res/raw' directory and play it. default: 'default' (default sound is played)
       number: 10, // (optional) Valid 32 bit integer specified as string. default: none (Cannot be zero)
-      repeatType: 'minute', // (optional) Repeating interval. Check 'Repeating Notifications' section for more info.
+      repeatType: 'week', // (optional) Repeating interval. Check 'Repeating Notifications' section for more info.
     });
     return;
     // const {Language} = configuration;
